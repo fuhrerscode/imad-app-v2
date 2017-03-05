b85aaea58020928f8e964559517f7ae5b4fb0964
fuhrerscode <rajeshtrinity27@gmail.com>
2017-03-05 10:05:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML = "New Value";
 //move the image
 var img = document.getElementById('img');
 img.onclick = function () {
-    img.style.marginLeft '100px';
+    img.style.marginLeft = '100px';
 };
