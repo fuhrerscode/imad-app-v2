076a9098d26c5e8f910d0ddd111da2ded5d0318a
fuhrerscode <rajeshtrinity27@gmail.com>
2017-03-08 09:28:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,28 @@
 // counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick= function(){
     
-    // make a request to counter end point
+    // create a request object
+    var request =new XMLHttpRequest();
+    
+    
     
     // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readtState == XMLHttpRequest.DONE) {
+            if (request.status == 200) {
+                var counter = request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML =counter;
+                
+            }
+        }
+        // not done
+    };
     
-    // render the variable in correct span
-    counter = counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML =counter;
+    // make a request
+    request.open('GET','http://fuhrerscode.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
