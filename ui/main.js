2dca4d587f608f34fe0f56027855760b5268583a
fuhrerscode <rajeshtrinity27@gmail.com>
2017-03-08 08:37:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,13 @@
-console.log('Loaded!');
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-// change the text of the main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft= marginLeft + 'px';
+button.onclick = function () {
+    
+    // make a request to counter end point
+    
+    // capture the response and store it in correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function () {
-    var interval = setInterva(moveRight, 50);
-   
-};
